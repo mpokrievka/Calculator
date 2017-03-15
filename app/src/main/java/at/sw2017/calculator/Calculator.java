41bed113e68ef832cc58d0eb8fd558c8d12a8138
mpokrievka <mmpokr@gmail.com>
2017-03-15 17:43:27 +0100

    [AU, MP] Implemented buttons

--- app/src/main/java/at/sw2017/calculator/Calculator.java
+++ app/src/main/java/at/sw2017/calculator/Calculator.java
@@ -1,13 +1,57 @@
 package at.sw2017.calculator;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Calculator extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class Calculator extends Activity implements View.OnClickListener {
+   // private Button buttonZero, buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix,
+   //         buttonSeven, buttonEight, buttonNine;
+    private Button buttonAdd, buttonSubstract, buttonMultiply, buttonDivide, buttonEqual;
+    private Button buttonC;
+    private ArrayList<Button> numberButtons = new ArrayList<Button>();
+
+    public void setUpNumberButtonListener(){
+        for(int i = 0; i<=9; i++){
+            String buttonName = "button" + i;
+
+            int id = getResources().getIdentifier(buttonName, "id", R.class.getPackage().getName());
+
+            Button button = (Button) findViewById(id);
+            button.setOnClickListener(this);
+
+            numberButtons.add(button);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonSubstract = (Button) findViewById(R.id.buttonSubstract);
+        buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        buttonEqual = (Button) findViewById(R.id.buttonEqual);
+        buttonC = (Button) findViewById(R.id.buttonC);
+
+        buttonAdd.setOnClickListener(this);
+        buttonSubstract.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        buttonDivide.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
+        buttonC.setOnClickListener(this);
+
+        setUpNumberButtonListener();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
